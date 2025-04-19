6a401c584f37f7a006071456323a73e02c0310ac
artem <artem-paramonchik@mail.ru>
2025-04-19 13:01:04 +0300

    Темная тема + картинки рабочие

--- app/src/main/java/com/example/booktracker/RecyclerView/BookHolder.kt
+++ app/src/main/java/com/example/booktracker/RecyclerView/BookHolder.kt
@@ -11,11 +11,12 @@ class BookHolder(private val binding: BookItemBinding): RecyclerView.ViewHolder(
 
     fun bind(book: Book) {
         binding.titleBook.text = book.title
-        binding.authorBook.text = book.authors?.first()?:"автор неизвестен"
-        binding.pagesBook.text = "Страниц: ${book.pageCount?:"неизвестно"}"
+        binding.authorBook.text = book.authors?.joinToString()?:"автор неизвестен"
+        val pagesText = "Страниц: ${book.pageCount?:"неизвестно"}"
+        binding.pagesBook.setText(pagesText)
 
         Glide.with(binding.root)
-            .load(book.imageLinks?.maybeNormImage)
+            .load(book.imageLinks?.maybeNormImage?.replace("http://", "https://"))
             .placeholder(R.drawable.ic_home_black_24dp)
             .into(binding.imageBook)
     }
